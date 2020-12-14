76d97d646225277aece25854adfcbea6f8782f69
Bozidar Dautovic <bdautovic@vast.com>
2020-12-14 18:15:04 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,38 @@
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# create projects directory
+mkdir ~/projects
+
+# install brews
+brew install node
+brew install siege
+brew install jq
+brew install httpie
+brew install noti
+brew install tig
+brew install ack
+brew install awscli
+brew install git
+brew install postgresql
+brew install mysql
+brew install uchardet
+
+# add taps
+brew tap homebrew/cask-versions
+
+# install casks
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install emacs
+brew cask install google-chrome
+brew cask install postman
+brew cask install grammarly
+brew cask install zeplin
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install npm packages
+npm i -g n
+npm i -g eslint
