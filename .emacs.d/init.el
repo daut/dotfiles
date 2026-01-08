836a570de803216c7a891412dcbb46a4e8a859b4
daut <bozidardautovic@gmail.com>
2026-01-08 12:49:01 +0100

    feat: start emacs server

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -50,6 +50,12 @@
   (auto-package-update-maybe)
   (auto-package-update-at-time "09:00"))
 
+(use-package server
+  :ensure nil
+  :config
+  (unless (server-running-p)
+    (server-start)))
+
 ;; Save all of the custom data in custom.el
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (when (file-exists-p custom-file)
